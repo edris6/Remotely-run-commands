8783a7578d23486d2113a6bfdf17d8ff2e910158
badonyt <soccerskin87@gmail.com>
2023-01-08 21:32:54 +0000

    Added repetion of commmands

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const app = express();
 const wss = new WebSocketServer({ port: 8080 });
 let prev_command = null
 let command = null
+let again = false;
 
 
 const interval = setInterval(function() {
   console.log(command)
   console.log(prev_command)
-  if (command != null && command != prev_command) {
+  if (command == null )return;
+    
+  if( command != prev_command || again == true) {
     console.log("ran")
+    again = false
     prev_command = command
     wss.clients.forEach(function (client) {
       client.send(command.toString());
@@ -34,6 +38,7 @@ app.post('/upload', function (req, res) {
     console.log("sucess")
     console.log(req.body.message)
     command = req.body.message
+    again = true
 });
 
 app.listen(3000)
